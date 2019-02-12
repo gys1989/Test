bb5faacaa8d848126f89c635a0ddbc4844b0897c
lbs <916124007@qq.com>
2019-02-12 10:41:10 +0800

    abcd

---
+++ Test_abcd/Test_abcd/main_vc/self_ViewController.h
@@ -0,0 +1,17 @@
+//
+//  self_ViewController.h
+//  Test_abcd
+//
+//  Created by 浪子 on 2019/2/12.
+//  Copyright © 2019 浪子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface self_ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test_abcd/Test_abcd/main_vc/self_ViewController.m
@@ -0,0 +1,32 @@
+//
+//  self_ViewController.m
+//  Test_abcd
+//
+//  Created by 浪子 on 2019/2/12.
+//  Copyright © 2019 浪子. All rights reserved.
+//
+
+#import "self_ViewController.h"
+
+@interface self_ViewController ()
+
+@end
+
+@implementation self_ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
